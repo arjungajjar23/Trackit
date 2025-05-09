b63554ee48300900d732de6e96ad8c98615282eb
Arjun Ambasana <arjun.gajjar23@gmail.com>
2025-05-09 14:12:08 -0400

    Initial commit

---
+++ TrackitTests/TrackitTests.swift
@@ -0,0 +1,16 @@
+//
+//  TrackitTests.swift
+//  TrackitTests
+//
+//  Created by Arjun Ambasana on 5/5/25.
+//
+
+import Testing
+
+struct TrackitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
